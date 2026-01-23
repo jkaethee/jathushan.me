bb66837454107e633c6cc8e2fd74d40d8768cb53
Jathushan Kaetheeswaran <jkaethee@Jathushans-MacBook-Air.local>
2026-01-23 16:01:45 -0500

    minor typos

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -10,11 +10,11 @@ function AboutPage() {
           About
         </div>
         <div className="aboutBody">
-          Hello! I'm Jathushan and I'm a 2nd year MASc Biomedical Engineering student at the University of Toronto. I'm currently affialted with both the KITE and Krembil Research Institutes with a focus on wearable technology around ECG, PPG, ACC, and EEG signals.
+          Hello! I'm Jathushan and I'm a 2nd year MASc Biomedical Engineering student at the University of Toronto. I'm currently affiliated with both the KITE and Krembil Research Institutes with a focus on wearable technology around ECG, PPG, ACC, and EEG signals.
           <br/>
           <p>
             My passions revolve around producing innovative solutions within the healthcare sector, and I'm always eager to tackle new challenges! I have extensive experience as a Full Stack Developer at several biotech companies and hospitals. More recently, I was the primary software engineer on the Topaz Photo AI desktop application based in Texas, where I applied these skills to lead the development work. I'm currently looking for full-time post-grad opportunities in the field of wearables!
-            Outside of my work and school life, I am interested in all forms of media: music, film, print, etc. I am also an avid NBA and (more recently) NFL fan, and love to partake in sports and athletics. I'm an avid Brazilian Jiu-Jitsu hobbyist and am a lifelong martial artist at heart <span aria-label="smiley" role="img">😃</span>
+            Outside of my work and school life, I am interested in all forms of media: music, film, print, etc. I am also an avid NBA and (more recently) NFL fan, and love to partake in sports and athletics. I'm an avid Brazilian Jiu-Jitsu hobbyist and am a lifelong martial artist at heart. <span aria-label="smiley" role="img">😃</span>
           </p>
         </div>
       </div>
